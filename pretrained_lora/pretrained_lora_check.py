8bb9aedea7da7c35d4da79534432befbec4ffc83
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-09-29 17:53:27 +0900

    Update pretrained_lora_check.py

--- pretrained_lora/pretrained_lora_check.py
+++ pretrained_lora/pretrained_lora_check.py
@@ -3,3 +3,5 @@ from safetensors.torch import load_file, safe_open
 
 pretrained_lora_dir = 'PlatformGameV0_2.safetensors'
 weights_sd = load_file(pretrained_lora_dir)
+for layer in weights_sd.keys():
+    print(layer)
