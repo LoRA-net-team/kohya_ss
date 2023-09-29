acc90a160779228cd801c75d907103ad2eebc3fd
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-09-29 17:56:34 +0900

    Update pretrained_lora_check.py

--- pretrained_lora/pretrained_lora_check.py
+++ pretrained_lora/pretrained_lora_check.py
@@ -4,4 +4,12 @@ from safetensors.torch import load_file, safe_open
 pretrained_lora_dir = 'PlatformGameV0_2.safetensors'
 weights_sd = load_file(pretrained_lora_dir)
 for layer in weights_sd.keys():
-    print(layer)
+
+    if "alpha" in layer :
+        alpha_value = weights_sd[layer]
+    elif "lora_down" in layer :
+        layer_name = layer.split('.lora')[0]
+        down_weight = weights_sd[layer]
+        up_layer_key = f'{layer_name}.lora_up.weight'
+        up_weight = weights_sd[up_layer_key]
+        print(f'down_weight : {down_weight.shape} | up_weight : {up_weight.shape}')
