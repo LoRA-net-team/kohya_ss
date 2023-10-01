1850b5d1c44ef1a2886bcfe0e5cf1165f8fa3e5b
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-10-01 23:15:55 +0900

    log loss change

--- train_network.py
+++ train_network.py
@@ -97,8 +97,8 @@ class NetworkTrainer:
 
     # TODO 他のスクリプトと共通化する
     def generate_step_logs(
-        self, args: argparse.Namespace, current_loss, avr_loss, lr_scheduler, keys_scaled=None, mean_norm=None, maximum_norm=None
-    ):
+        self, args: argparse.Namespace, current_loss, avr_loss, lr_scheduler, keys_scaled=None, mean_norm=None, maximum_norm=None,
+            task_loss=None, attn_loss=None,):
         logs = {"loss/current": current_loss,
                 "loss/average": avr_loss,
                 "loss/task_loss": task_loss.item(),
