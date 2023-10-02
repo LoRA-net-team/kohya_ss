c6454bff1df9e62b00fcc255d6cf7e5f0bddb7c8
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-10-02 13:41:47 +0900

    Update train_network.py

--- train_network.py
+++ train_network.py
@@ -902,6 +902,7 @@ class NetworkTrainer:
                     layer_names = atten_collection.keys()
                     map_dict = {}
                     for layer_name in layer_names:
+                        print(f'from collection, layer_name: {layer_name}')
                         attn_list = atten_collection[layer_name] # just one map element
                         attn_map = attn_list[0]                  # [Batch*8, pix_len, sen_len]
                         batch_attn_map = torch.chunk(attn_map, len(trg_indexs), dim=0)
