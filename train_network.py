2426550d0b6536f0ee049d57058ef555a53f633d
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-10-05 09:53:25 +0900

    only_shallow_attn_map

--- train_network.py
+++ train_network.py
@@ -74,8 +74,9 @@ def register_attention_control(unet : nn.Module, controller):
             # ----------------------------------------------------------------------------------------------------------------
             if is_cross_attention:
                 factor = int(math.sqrt(512 // attention_probs.shape[1]))
+                if attention_probs.shape[1] == 4096 :
                 #print(f'{layer_name} attention_probs.shape : {attention_probs.shape}')
-                attn = controller.store(attention_probs, layer_name)
+                    attn = controller.store(attention_probs, layer_name)
             # 2) after value calculating
             hidden_states = torch.bmm(attention_probs, value)
             hidden_states = self.reshape_batch_dim_to_heads(hidden_states)
