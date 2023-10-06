8a7d87f20e9bee09884eabbacc5774f2e68afc49
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-10-06 18:53:58 +0900

    .

--- library/train_util.py
+++ library/train_util.py
@@ -1216,6 +1216,7 @@ class BaseDataset(torch.utils.data.Dataset):
                             trg_indexs = []
                             for i, id in enumerate(token_id):
                                 if id in trg_token_id:
+                                    print(f'id to save : {id}')
                                     trg_indexs.append(i)
                         return trg_indexs
 
