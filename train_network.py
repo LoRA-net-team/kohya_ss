623910f05ec7ecf55f62dcc577cf5b5704d1ba85
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-10-08 23:35:02 +0900

    loss recording

--- train_network.py
+++ train_network.py
@@ -916,7 +916,9 @@ class NetworkTrainer:
                             layer_names = atten_collection.keys()
                             attn_loss = 0
                             for layer_name in layer_names:
+                                a = sum(atten_collection[layer_name])
                                 attn_loss = attn_loss + sum(atten_collection[layer_name])
+                                f.write(f'{layer_name} : {a.item()}\n')
                             loss = task_loss + args.attn_loss_ratio * attn_loss
                         accelerator.backward(loss)
                         if accelerator.sync_gradients and args.max_grad_norm != 0.0:
