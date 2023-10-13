fe897c0ddf2ff62ad7c82b081c3733a3b5d777c4
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-10-13 10:55:50 +0900

    erase printing index list

--- train_network_sy.py
+++ train_network_sy.py
@@ -851,7 +851,6 @@ class NetworkTrainer:
                                                     weight_dtype,
                                                     batch["trg_indexs_list"],
                                                     batch['mask_imgs'])
-                        print(f'in call unet, batch["trg_indexs_list"] : {batch["trg_indexs_list"]}')
                         atten_collection = attention_storer.step_store
                         attention_storer.step_store = {}
 
