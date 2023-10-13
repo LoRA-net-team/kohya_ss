684ad733f09202775d6cb470727ba045aea0cf97
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-10-13 15:49:46 +0900

    check mask dir

--- generate_crossattn_map.sh
+++ generate_crossattn_map.sh
@@ -1,24 +1,28 @@
+CUDA_VISIBLE_DEVICES=0 python gen_crossattnmap.py \
+      --ckpt /data7/sooyeon/LyCORIS/LyCORIS/pretrained/animefull-final-pruned-fp16.safetensors \
+      --network_module networks.lora \
+      --network_weights './result/haibara_experience/haibara_3_one_image_base/haibara-000033.safetensors' \
+      --from_file '/data7/sooyeon/LyCORIS/LyCORIS/test/test_haibara_inference.txt' \
+      --outdir attn_test/20231013_result/haibara_3_one_image_base/haibara --seed 42 --trg_token 'haibara'
+# ------------------------------------------------------------------------------------------------------------------------
+CUDA_VISIBLE_DEVICES=1 python gen_crossattnmap.py \
+      --ckpt /data7/sooyeon/LyCORIS/LyCORIS/pretrained/animefull-final-pruned-fp16.safetensors \
+      --network_module networks.lora \
+      --network_weights './result/haibara_experience/haibara_3_one_image_mask/haibara-000059.safetensors' \
+      --from_file '/data7/sooyeon/LyCORIS/LyCORIS/test/test_haibara_inference.txt' \
+      --outdir attn_test/20231013_result/haibara_3_one_image_mask/haibara --seed 42 --trg_token 'haibara'
+# ------------------------------------------------------------------------------------------------------------------------
 CUDA_VISIBLE_DEVICES=2 python gen_crossattnmap.py \
       --ckpt /data7/sooyeon/LyCORIS/LyCORIS/pretrained/animefull-final-pruned-fp16.safetensors \
       --network_module networks.lora \
-      --network_weights './result/jungwoo_experience/jungwoo_3_attn_loss_layerwise_detail/jungwoo-000009.safetensors' \
-      --from_file '/data7/sooyeon/LyCORIS/LyCORIS/test/test_jungwoo_inference.txt' \
-      --outdir 20231011_result/jungwoo_3_attn_loss_layerwise_detail/jw --seed 42 --trg_token 'jw'
-
-# ---------------------------------------------------------------------------------------------------------------------------------------
+      --network_weights './result/haibara_experience/haibara_3_four_image_base/haibara-000045.safetensors' \
+      --from_file '/data7/sooyeon/LyCORIS/LyCORIS/test/test_haibara_inference.txt' \
+      --outdir attn_test/20231013_result/haibara_3_four_image_base/haibara --seed 42 --trg_token 'haibara'
+# ------------------------------------------------------------------------------------------------------------------------
 CUDA_VISIBLE_DEVICES=3 python gen_crossattnmap.py \
       --ckpt /data7/sooyeon/LyCORIS/LyCORIS/pretrained/animefull-final-pruned-fp16.safetensors \
       --network_module networks.lora \
-      --network_weights './result/jungwoo_experience/jungwoo_3_attn_loss_test_2/jungwoo-000016.safetensors' \
-      --from_file '/data7/sooyeon/LyCORIS/LyCORIS/test/test_jungwoo_inference.txt' \
-      --outdir 20231011_result/jungwoo_3_attn_loss_test_2/jw --seed 42 --trg_token 'jw'
-# ---------------------------------------------------------------------------------------------------------------------------------------
-
-
-
-
-CUDA_VISIBLE_DEVICES=3 python generate_crossattn_map.py \
-      --ckpt /data7/sooyeon/LyCORIS/LyCORIS/pretrained/animefull-final-pruned-fp16.safetensors \
-
-
-      --prompt "jw, male_focused, center, smiling, wearing_shirt" --outdir 20231011_result/jungwoo_3_blur_mask_10_mean/jw --seed 42 --trg_token 'jw'
+      --network_weights './result/haibara_experience/haibara_3_four_image_mask/haibara-000037.safetensors' \
+      --from_file '/data7/sooyeon/LyCORIS/LyCORIS/test/test_haibara_inference.txt' \
+      --outdir attn_test/20231013_result/haibara_3_four_image_mask/haibara --seed 42 --trg_token 'haibara'
+# ------------------------------------------------------------------------------------------------------------------------
--- train_network_sy.py
+++ train_network_sy.py
@@ -82,10 +82,10 @@ def register_attention_control(unet : nn.Module, controller):
                             # ------------------------------------------------------------------------------------------------------------------------------
                             # mask = [512,512]
                             mask_ = mask[batch_idx].to(attention_prob.dtype) # (512,512)
-                            masked_heat_map = word_heat_map_ * mask_
+                            #masked_heat_map = word_heat_map_ * mask_
 
-                            attn_loss = F.mse_loss(word_heat_map_, masked_heat_map)
-                            controller.store(attn_loss, layer_name)
+                            #attn_loss = F.mse_loss(word_heat_map_, masked_heat_map)
+                            #controller.store(attn_loss, layer_name)
 
             hidden_states = torch.bmm(attention_probs, value)
             hidden_states = self.reshape_batch_dim_to_heads(hidden_states)
