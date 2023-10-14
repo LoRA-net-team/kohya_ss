e5f3c4fbd003e83aa5ef3eb2b065b3b48f0966e4
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-10-14 20:23:08 +0900

    change check

--- train_network_sy.py
+++ train_network_sy.py
@@ -202,6 +202,7 @@ class NetworkTrainer:
         train_util.sample_images(accelerator, args, epoch, global_step, device, vae, tokenizer, text_encoder, unet)
 
     def train(self, args):
+        print('start')
         if args.process_title :
             setproctitle(args.process_title)
         else :
