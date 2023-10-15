5cff5ed2b8389eb8fd983c65998a3bb343ffc4dc
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-10-15 22:20:10 +0900

    heatmap following class token

--- gen_crossattnmap.sh
+++ gen_crossattnmap.sh
@@ -1,15 +1,31 @@
 CUDA_VISIBLE_DEVICES=0 python gen_crossattnmap.py \
       --ckpt /data7/sooyeon/LyCORIS/LyCORIS/pretrained/animefull-final-pruned-fp16.safetensors \
       --network_module networks.lora \
-      --network_weights './result/haibara_experience/one_image/haibara_3_1_image_base/haibara-000026.safetensors' \
+      --network_weights './result/haibara_experience/one_image/haibara_3_1_image_continuous_binary_mask_ratio_10/haibara-000038.safetensors' \
       --from_file '/data7/sooyeon/LyCORIS/LyCORIS/test/test_haibara_inference.txt' \
-      --outdir 20231011_result/haibara_experience/haibara_3_1_image_base/haibara_epoch_26 --seed 42 --trg_token 'haibara' \
+      --outdir ./result/haibara_experience/one_image/haibara_3_1_image_continuous_binary_mask_ratio_10/inference_attention/haibara_epoch_38 --seed 42 --trg_token 'haibara' \
       --negative_prompt 'worst quality, mutated hand, blurry'
-#---------------
-CUDA_VISIBLE_DEVICES=0 python gen_crossattnmap.py \
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+CUDA_VISIBLE_DEVICES=1 python gen_crossattnmap.py \
       --ckpt /data7/sooyeon/LyCORIS/LyCORIS/pretrained/animefull-final-pruned-fp16.safetensors \
       --network_module networks.lora \
-      --network_weights './result/haibara_experience/one_image/haibara_3_1_image_continuous_binary_mask/haibara-000035.safetensors' \
+      --network_weights './result/haibara_experience/four_image/haibara_3_4_image_continuous_binary_mask/haibara-000121.safetensors' \
       --from_file '/data7/sooyeon/LyCORIS/LyCORIS/test/test_haibara_inference.txt' \
-      --outdir ./result/haibara_experience/one_image/haibara_3_1_image_continuous_binary_mask/inference_attention/haibara_epoch_35 --seed 42 --trg_token 'haibara' \
+      --outdir ./result/haibara_experience/four_image/haibara_3_4_image_continuous_binary_mask/inference_attention/haibara_epoch_121 --seed 42 --trg_token 'haibara' \
+      --negative_prompt 'worst quality, mutated hand, blurry'
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+CUDA_VISIBLE_DEVICES=2 python gen_crossattnmap.py \
+      --ckpt /data7/sooyeon/LyCORIS/LyCORIS/pretrained/animefull-final-pruned-fp16.safetensors \
+      --network_module networks.lora \
+      --network_weights './result/haibara_experience/four_image/haibara_3_4_image_continuous_mask/haibara-000142.safetensors' \
+      --from_file '/data7/sooyeon/LyCORIS/LyCORIS/test/test_haibara_inference.txt' \
+      --outdir ./result/haibara_experience/four_image/haibara_3_4_image_continuous_mask/inference_attention/haibara_epoch_142 --seed 42 --trg_token 'haibara' \
+      --negative_prompt 'worst quality, mutated hand, blurry'
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+CUDA_VISIBLE_DEVICES=3 python gen_crossattnmap.py \
+      --ckpt /data7/sooyeon/LyCORIS/LyCORIS/pretrained/animefull-final-pruned-fp16.safetensors \
+      --network_module networks.lora \
+      --network_weights './result/haibara_experience/four_image/haibara_3_4_image_continuous_mask_preserving/haibara-000121.safetensors' \
+      --from_file '/data7/sooyeon/LyCORIS/LyCORIS/test/test_haibara_inference.txt' \
+      --outdir ./result/haibara_experience/four_image/haibara_3_4_image_continuous_mask_preserving/inference_attention/haibara_epoch_121 --seed 42 --trg_token 'haibara' \
       --negative_prompt 'worst quality, mutated hand, blurry'
