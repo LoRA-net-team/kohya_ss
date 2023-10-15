980de3aa4c98bbce7406a98b1d695d3a4a14e987
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-10-15 14:10:55 +0900

    check negative prompt

--- gen_crossattnmap.sh
+++ gen_crossattnmap.sh
@@ -1,13 +1,16 @@
-CUDA_VISIBLE_DEVICES=4 python gen_crossattnmap.py \
+CUDA_VISIBLE_DEVICES=0 python gen_crossattnmap.py \
       --ckpt /data7/sooyeon/LyCORIS/LyCORIS/pretrained/animefull-final-pruned-fp16.safetensors \
       --network_module networks.lora \
-      --network_weights './result/jungwoo_experience/jungwoo_3_base/jungwoo-000013.safetensors' \
-      --from_file '/data7/sooyeon/LyCORIS/LyCORIS/test/test_jungwoo_inference.txt' \
-      --outdir 20231011_result/jungwoo_3_base/jw --seed 42 --trg_token 'jw'
+      --network_weights './result/haibara_experience/one_image/haibara_3_1_image_base/haibara-000026.safetensors' \
+      --from_file '/data7/sooyeon/LyCORIS/LyCORIS/test/test_haibara_inference.txt' \
+      --outdir 20231011_result/haibara_experience/haibara_3_1_image_base/haibara_epoch_26 --seed 42 --trg_token 'haibara' \
+      --negative_scale 7.5 --negative_prompt
+      --negative_prompt
 
-CUDA_VISIBLE_DEVICES=5 python gen_crossattnmap.py \
+#---------------
+CUDA_VISIBLE_DEVICES=0 python gen_crossattnmap.py \
       --ckpt /data7/sooyeon/LyCORIS/LyCORIS/pretrained/animefull-final-pruned-fp16.safetensors \
       --network_module networks.lora \
-      --network_weights './result/jungwoo_experience/jungwoo_3_blur_mask_10_mean/jungwoo-000015.safetensors' \
-      --from_file '/data7/sooyeon/LyCORIS/LyCORIS/test/test_jungwoo_inference.txt' \
-      --outdir 20231011_result/jungwoo_3_blur_mask_10_mean/jw --seed 42 --trg_token 'jw'
+      --network_weights './result/haibara_experience/one_image/haibara_3_1_image_base/haibara-000026.safetensors' \
+      --from_file '/data7/sooyeon/LyCORIS/LyCORIS/test/test_haibara_inference.txt' \
+      --outdir 20231011_result/haibara_experience/haibara_3_1_image_base/haibara_epoch_26 --seed 42 --trg_token 'haibara'
