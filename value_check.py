c09846d2ceac75fc7d0e15c0baaa9018727de941
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-10-20 21:31:48 +0900

    Update value_check.py

--- value_check.py
+++ value_check.py
@@ -6,7 +6,9 @@ trg_file = 'up_blocks_1_attentions_0_transformer_blocks_0_attn2_what_map.pt'
 what_map_torch = torch.load(trg_file)
 uncon, con = what_map_torch.chunk(2, dim=0)
 print(f'what_map_torch : {what_map_torch.shape}')
-pix_len = con.shape[1]
+b, pix_len, dim = con.shape
 res = math.sqrt(pix_len)
-re_con = rearrange(con, 'b (r r) c -> b r r w')
+
+
+re_con = con.reshape(b, res, res, dim)
 print(f're_con : {re_con.shape}')
