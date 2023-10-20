f687b0677458138f56b0838e1d911f2d68cb8dbe
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-10-20 21:28:59 +0900

    recon check

--- value_check.py
+++ value_check.py
@@ -1,8 +1,12 @@
 import os
-import torch
+import torch, math
+from einops import rearrange
 
 trg_file = 'up_blocks_1_attentions_0_transformer_blocks_0_attn2_what_map.pt'
 what_map_torch = torch.load(trg_file)
 uncon, con = what_map_torch.chunk(2, dim=0)
 print(f'what_map_torch : {what_map_torch.shape}')
-print(f'uncon : {uncon.shape}')
+pix_len = con.shape[1]
+res = math.sqrt(pix_len)
+re_con = rearrange(con, 'b (r r) c -> b r r w')
+print(f're_con : {re_con.shape}')
