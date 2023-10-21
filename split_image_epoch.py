587c2bfac0d831b0f142caf16c9a66413f75fb73
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-10-21 22:55:01 +0900

    split image epoch

--- split_image_epoch.py
+++ split_image_epoch.py
@@ -8,19 +8,21 @@ def extract_pure_name(img_dir) :
 def main(args) :
 
     print(f' step 1. call generated image folder')
-    generated_image_folder = args.generated_image_folder
-    sample_dir = os.path.join(generated_image_folder, 'sample')
-    images = os.listdir(sample_dir)
-    for image in images :
-        image_dir = os.path.join(sample_dir, image)
-        name, ext = os.path.splitext(image_dir)
-        epoch = name.split('_')[-2]
-        epoch = int(epoch.split('e')[-1])
+    generated_image_folders = os.listdir(args.generated_image_folder)
+    for generated_image_folder in generated_image_folders :
+        generated_image_folder_dir = os.path.join(args.generated_image_folder, generated_image_folder)
+        sample_dir = os.path.join(generated_image_folder_dir, 'sample')
+        images = os.listdir(sample_dir)
+        for image in images :
+            image_dir = os.path.join(sample_dir, image)
+            name, ext = os.path.splitext(image_dir)
+            epoch = name.split('_')[-2]
+            epoch = int(epoch.split('e')[-1])
 
-        epoch_dir = os.path.join(sample_dir, str(epoch))
-        os.makedirs(epoch_dir, exist_ok=True)
-        new_dir = os.path.join(epoch_dir, image)
-        os.rename(image_dir, new_dir)
+            epoch_dir = os.path.join(sample_dir, str(epoch))
+            os.makedirs(epoch_dir, exist_ok=True)
+            new_dir = os.path.join(epoch_dir, image)
+            os.rename(image_dir, new_dir)
 
 
 
@@ -28,6 +30,6 @@ def main(args) :
 if __name__ == '__main__' :
     parser = argparse.ArgumentParser()
     parser.add_argument('--generated_image_folder',
-                        default='./result/haibara_experience/one_image/name_3_without_caption/haibara_base')
+                        default='/data7/sooyeon/LyCORIS/lyco_v2/kohya_ss/result/perfusion_data_experience/one_image/mcat_without_caption')
     args = parser.parse_args()
     main(args)
