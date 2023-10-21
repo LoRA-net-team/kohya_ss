98c4324c1b8b166de7e99d27c31864033b30aec7
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-10-21 23:30:53 +0900

    Update clip_score.py

--- Dino_Scoring/clip_score.py
+++ Dino_Scoring/clip_score.py
@@ -8,7 +8,7 @@ import clip
 from PIL import Image, ImageFile
 
 from aesthetic_model import Aesthetic_MLP, normalized
-import VITs16, get_dino_dim
+from Dino_Scoring import VITs16, get_dino_dim
 import time
 
 def compute_cosine_distance(image_features, image_features2):
