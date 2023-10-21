878a8a8460f982053a1ffd84c921931f8c0f26e6
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-10-21 21:33:05 +0900

    wandb reset

--- train_network_sy.py
+++ train_network_sy.py
@@ -322,11 +322,10 @@ class NetworkTrainer:
         print("preparing accelerator")
         accelerator = train_util.prepare_accelerator(args)
         is_main_process = accelerator.is_main_process
-        if args.log_with == 'wandb' :
+        if args.log_with == 'wandb' and is_main_process :
             import wandb
             wandb.init(project=args.wandb_init_name,
                        name=args.wandb_run_name)
-
 
 
         save_base_dir = args.output_dir
