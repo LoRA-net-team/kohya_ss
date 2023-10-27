9a56eeeb115ecea0acc6240578b325c2e7794aa9
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-10-27 19:12:03 +0900

    add dino folder

---
+++ Dino_Scoring/clip_score.sh
@@ -0,0 +1,4 @@
+python clip_score.py --ref_img_folder /data7/sooyeon/MyData/perfusion_dataset/iom_200/200_iom \
+                     --gen_img_folder /data7/sooyeon/LyCORIS/lyco_v2/kohya_ss/result/iom_experiment/one_image/iom_image \
+                     --trg_condition 'iom_te_pretraining_test' \
+                     --concept_token 'iom' --class_token cat
