c9f2f35758776d7fd187607607b80f67a86a0735
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-10-29 14:48:46 +0900

    Update image_inverting.py

--- image_inverting.py
+++ image_inverting.py
@@ -233,6 +233,7 @@ def main(args) :
         next_sample = alpha_prod_t_next ** 0.5 * next_original_sample + next_sample_direction
         return next_sample
 
+    scheduler.set_timesteps(NUM_DDIM_STEPS)
     range_timesteps = len(scheduler.timesteps)
     print(f'len of scheduler timesteps : {range_timesteps}')
     @torch.no_grad()
