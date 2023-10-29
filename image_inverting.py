654eb885b006ac1c443ba691481d4d5ca69776a6
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-10-29 19:52:16 +0900

    trimming

--- image_inverting.py
+++ image_inverting.py
@@ -1,4 +1,3 @@
-# get image vector with semantic from each time step and spatial layer block position
 import importlib
 import argparse
 import gc
@@ -30,6 +29,10 @@ import torch.nn.functional as F
 from functools import lru_cache
 from attention_store import AttentionStore
 import wandb
+import numpy as np
+from PIL import Image
+from typing import Union
+from library.lpw_stable_diffusion import StableDiffusionLongPromptWeightingPipeline
 try:
     from setproctitle import setproctitle
 except (ImportError, ModuleNotFoundError):
@@ -41,15 +44,8 @@ try:
         ipex_init()
 except Exception:
     pass
-from diffusers import (StableDiffusionPipeline,DDPMScheduler,EulerAncestralDiscreteScheduler,DPMSolverMultistepScheduler,
-                       DPMSolverSinglestepScheduler,LMSDiscreteScheduler,PNDMScheduler,DDIMScheduler,
-                       EulerDiscreteScheduler,HeunDiscreteScheduler,KDPM2DiscreteScheduler,KDPM2AncestralDiscreteScheduler,
-                       AutoencoderKL,)
-import numpy as np
-from PIL import Image
-from typing import Union
-from library.lpw_stable_diffusion import StableDiffusionLongPromptWeightingPipeline
-
+from diffusers import (DDPMScheduler,EulerAncestralDiscreteScheduler,DPMSolverMultistepScheduler, DPMSolverSinglestepScheduler,
+                       LMSDiscreteScheduler,PNDMScheduler,DDIMScheduler, EulerDiscreteScheduler,HeunDiscreteScheduler,KDPM2DiscreteScheduler,KDPM2AncestralDiscreteScheduler)
 
 def register_attention_control(unet : nn.Module, controller:AttentionStore) :
     """ Register cross attention layers to controller. """
@@ -210,8 +206,7 @@ def call_unet(unet, noisy_latents, timesteps, text_conds, trg_indexs_list, mask_
                       mask_imgs=mask_imgs, ).sample
     return noise_pred
 
-def next_step(model_output: Union[torch.FloatTensor, np.ndarray],timestep: int,
-              sample: Union[torch.FloatTensor, np.ndarray],scheduler):
+def next_step(model_output: Union[torch.FloatTensor, np.ndarray],timestep: int, sample: Union[torch.FloatTensor, np.ndarray],scheduler):
     timestep, next_timestep = min( timestep - scheduler.config.num_train_timesteps // scheduler.num_inference_steps, 999), timestep
     alpha_prod_t = scheduler.alphas_cumprod[timestep] if timestep >= 0 else scheduler.final_alpha_cumprod
     alpha_prod_t_next = scheduler.alphas_cumprod[next_timestep]
@@ -234,7 +229,6 @@ def ddim_loop(latent, context, NUM_DDIM_STEPS, scheduler, unet):
         latent = next_step(noise_pred, t, latent, scheduler)
         all_latent.append(latent)
     return all_latent, time_steps
-
 
 @torch.no_grad()
 def latent2image(latents, vae, return_type='np'):
@@ -302,10 +296,8 @@ def main(args) :
     tokenizers = tokenizer if isinstance(tokenizer, list) else [tokenizer]
 
     print(f' (1.2) SD')
-    #text_encoder, vae, unet, _ = train_util._load_target_model(args, weight_dtype, accelerator)
     text_encoder, vae, unet, load_stable_diffusion_format = train_util._load_target_model(args,weight_dtype,args.device,
                                                                                           unet_use_linear_projection_in_v2=False,)
-    model_version = model_util.get_model_version_str_for_sd1_sd2(args.v2, args.v_parameterization)
     text_encoders = text_encoder if isinstance(text_encoder, list) else [text_encoder]
 
     print(f' (1.3) register attention storer')
@@ -314,29 +306,19 @@ def main(args) :
 
     print(f' (1.4) scheduler')
     sched_init_args = {}
-    if args.sample_sampler == "ddim":
-        scheduler_cls = DDIMScheduler
-    elif args.sample_sampler == "ddpm":  # ddpmはおかしくなるのでoptionから外してある
-        scheduler_cls = DDPMScheduler
-    elif args.sample_sampler == "pndm":
-        scheduler_cls = PNDMScheduler
-    elif args.sample_sampler == "lms" or args.sample_sampler == "k_lms":
-        scheduler_cls = LMSDiscreteScheduler
-    elif args.sample_sampler == "euler" or args.sample_sampler == "k_euler":
-        scheduler_cls = EulerDiscreteScheduler
-    elif args.sample_sampler == "euler_a" or args.sample_sampler == "k_euler_a":
-        scheduler_cls = EulerAncestralDiscreteScheduler
+    if args.sample_sampler == "ddim": scheduler_cls = DDIMScheduler
+    elif args.sample_sampler == "ddpm": scheduler_cls = DDPMScheduler
+    elif args.sample_sampler == "pndm": scheduler_cls = PNDMScheduler
+    elif args.sample_sampler == "lms" or args.sample_sampler == "k_lms": scheduler_cls = LMSDiscreteScheduler
+    elif args.sample_sampler == "euler" or args.sample_sampler == "k_euler": scheduler_cls = EulerDiscreteScheduler
+    elif args.sample_sampler == "euler_a" or args.sample_sampler == "k_euler_a": scheduler_cls = EulerAncestralDiscreteScheduler
     elif args.sample_sampler == "dpmsolver" or args.sample_sampler == "dpmsolver++":
         scheduler_cls = DPMSolverMultistepScheduler
         sched_init_args["algorithm_type"] = args.sample_sampler
-    elif args.sample_sampler == "dpmsingle":
-        scheduler_cls = DPMSolverSinglestepScheduler
-    elif args.sample_sampler == "heun":
-        scheduler_cls = HeunDiscreteScheduler
-    elif args.sample_sampler == "dpm_2" or args.sample_sampler == "k_dpm_2":
-        scheduler_cls = KDPM2DiscreteScheduler
-    elif args.sample_sampler == "dpm_2_a" or args.sample_sampler == "k_dpm_2_a":
-        scheduler_cls = KDPM2AncestralDiscreteScheduler
+    elif args.sample_sampler == "dpmsingle": scheduler_cls = DPMSolverSinglestepScheduler
+    elif args.sample_sampler == "heun": scheduler_cls = HeunDiscreteScheduler
+    elif args.sample_sampler == "dpm_2" or args.sample_sampler == "k_dpm_2": scheduler_cls = KDPM2DiscreteScheduler
+    elif args.sample_sampler == "dpm_2_a" or args.sample_sampler == "k_dpm_2_a": scheduler_cls = KDPM2AncestralDiscreteScheduler
     else:
         scheduler_cls = DDIMScheduler
     if args.v_parameterization:
@@ -362,40 +344,31 @@ def main(args) :
 
     print(f' \n step 2. groundtruth image preparing')
     print(f' (2.1) prompt condition')
-    prompt = 'teddy bear, wearing sunglasses'
+    prompt = args.prompt
     context = init_prompt(tokenizer, text_encoder, device, prompt)
 
     print(f' (2.2) image condition')
-    init_image_dir = '/data7/sooyeon/MyData/perfusion_dataset/td_100/100_td/td_1.jpg'
-    image_gt_np = load_512(init_image_dir)
+    image_gt_np = load_512(args.concept_image)
 
     print(f' \n step 3. image inverting')
     latent = image2latent(image_gt_np, vae, device, weight_dtype)
-    NUM_DDIM_STEPS = 30
-    scheduler.set_timesteps(NUM_DDIM_STEPS)
-    ddim_latents, time_steps = ddim_loop(latent, context, NUM_DDIM_STEPS, scheduler, unet)
-
+    scheduler.set_timesteps(args.num_ddim_steps)
+    ddim_latents, time_steps = ddim_loop(latent, context, args.num_ddim_steps, scheduler, unet)
     layer_names = attention_storer.self_query_store.keys()
-
     self_query_collection = attention_storer.self_query_store
     self_key_collection = attention_storer.self_key_store
     self_value_collection = attention_storer.self_value_store
-
     self_query_dict, self_key_dict, self_value_dict = {}, {}, {}
     cross_query_dict, cross_key_dict = {}, {}
 
     for layer in layer_names:
-
         self_query_list = attention_storer.self_query_store[layer]
         self_key_list = attention_storer.self_key_store[layer]
         self_value_list = attention_storer.self_value_store[layer]
-
         cross_layer = layer.replace('attn1', 'attn2')
         cross_query_list = attention_storer.cross_query_store[cross_layer]
         cross_key_list = attention_storer.cross_key_store[cross_layer]
-
         i = 0
-
         for self_query, self_key, self_value, cross_query, cross_key in zip(self_query_list,self_key_list,self_value_list,
                                                                             cross_query_list,cross_key_list) :
             time_step = time_steps[i]
@@ -426,19 +399,19 @@ def main(args) :
     print(f' \n step 3. generating image')
     vae.to(device)
     if hasattr(scheduler.config, "clip_sample") and scheduler.config.clip_sample is False: scheduler.config.clip_sample = True
-    pipeline = StableDiffusionLongPromptWeightingPipeline(text_encoder=text_encoder, vae=vae, unet=unet, tokenizer=tokenizer,scheduler=scheduler,safety_checker=None, feature_extractor=None,
+    pipeline = StableDiffusionLongPromptWeightingPipeline(text_encoder=text_encoder, vae=vae, unet=unet, tokenizer=tokenizer,scheduler=scheduler,
+                                                          safety_checker=None, feature_extractor=None,
                                                           requires_safety_checker=False,clip_skip=args.clip_skip, )
     pipeline.to(device)
     unregister_attention_control(unet, attention_storer)
 
     with torch.no_grad():
-        prompt = prompt
-        negative_prompt = 'low quality, worst quality, bad anatomy,bad composition, poor, low effort'
+        prompt = args.prompt
+        negative_prompt = args.negative_prompt
         image = None
         mask_image = None
         height = 512
         width = 512
-        num_inference_steps = 30
         guidance_scale = 8
         strength = 0.8
         num_images_per_prompt = 1
@@ -463,13 +436,12 @@ def main(args) :
         dtype = text_embeddings.dtype
 
         # 5. set timesteps
-        pipeline.scheduler.set_timesteps(num_inference_steps, device=device)
-        timesteps, num_inference_steps = pipeline.get_timesteps(num_inference_steps, strength, device, image is None)
+        pipeline.scheduler.set_timesteps(args.num_ddim_steps, device=device)
+        timesteps, num_inference_steps = pipeline.get_timesteps(args.num_ddim_steps, strength, device, image is None)
         latent_timestep = timesteps[:1].repeat(batch_size * num_images_per_prompt)
 
         # 6. Prepare latent variables
-        latents, init_latents_orig, noise = pipeline.prepare_latents(image, latent_timestep,
-                                                                     batch_size * num_images_per_prompt,
+        latents, init_latents_orig, noise = pipeline.prepare_latents(image, latent_timestep, batch_size * num_images_per_prompt,
                                                                      height, width,dtype, device, generator, latents,)
 
         # 7. Prepare extra step kwargs. TODO: Logic should ideally just be moved out of the pipeline
@@ -480,8 +452,6 @@ def main(args) :
         max_self_input_time = 10
         for i, t in enumerate(pipeline.progress_bar(timesteps)):
             save_time = int(t.item())-1
-            print(f't : {t} : save_time : {save_time}')
-
             # expand the latents if we are doing classifier free guidance
             latent_model_input = torch.cat([latents] * 2) if do_classifier_free_guidance else latents
             latent_model_input = pipeline.scheduler.scale_model_input(latent_model_input, t)
@@ -498,23 +468,20 @@ def main(args) :
                 self_input_time += 1
             else :
                 noise_pred = unet(latent_model_input, t, encoder_hidden_states=text_embeddings,).sample
-
             # perform guidance
             if do_classifier_free_guidance:
                 noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
                 noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
             latents = pipeline.scheduler.step(noise_pred, t, latents, **extra_step_kwargs).prev_sample
-
             if i % callback_steps == 0:
                 if callback is not None:
                     callback(i, t, latents)
                 if is_cancelled_callback is not None and is_cancelled_callback():
                     return None
         image = pipeline.latents_to_image(latents)[0]
-        image_save_dir = os.path.join(args.output_dir, f'self_new_condition_image.jpg')
+        prompt_save_name = prompt.replace(' ','_')
+        image_save_dir = os.path.join(args.output_dir, f'{prompt_save_name}.jpg')
         image.save(image_save_dir)
-
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -525,8 +492,7 @@ if __name__ == "__main__":
     config_util.add_config_arguments(parser)
     custom_train_functions.add_custom_train_arguments(parser)
     parser.add_argument("--device", type=str, default="cuda")
-    parser.add_argument("--save_model_as", type=str, default="safetensors",
-                        choices=[None, "ckpt", "pt", "safetensors"],
+    parser.add_argument("--save_model_as", type=str, default="safetensors", choices=[None, "ckpt", "pt", "safetensors"],
                         help="format to save the model (default is .safetensors) / モデル保存時の形式（デフォルトはsafetensors）", )
     parser.add_argument("--unet_lr", type=float, default=None, help="learning rate for U-Net / U-Netの学習率")
     parser.add_argument("--text_encoder_lr", type=float, default=None,
@@ -563,27 +529,14 @@ if __name__ == "__main__":
     parser.add_argument("--wandb_init_name", type=str)
     parser.add_argument("--wandb_log_template_path", type=str)
     parser.add_argument("--wandb_key", type=str)
-    parser.add_argument("--trg_concept", type=str, default='haibara')
-    parser.add_argument("--net_key_names", type=str, default='text')
-
     # class_caption
-    parser.add_argument("--class_caption", type=str, default='girl')
-    parser.add_argument("--heatmap_loss", action='store_true')
-    parser.add_argument("--attn_loss_ratio", type=float, default=1.0)
-    parser.add_argument("--mask_dir", type=str)
-
-    # masked_loss
-    parser.add_argument("--masked_loss", action='store_true')
-    parser.add_argument("--only_second_training", action='store_true')
-    parser.add_argument("--only_third_training", action='store_true')
-    parser.add_argument("--first_second_training", action='store_true')
-    parser.add_argument("--second_third_training", action='store_true')
-    parser.add_argument("--first_second_third_training", action='store_true')
-    parser.add_argument("--attn_loss_layers", type=str, default="all", help="attn loss layers, can be splitted with ',', matches regex with given string. default is 'all'")
-    # mask_threshold (0~1, default 1)
-    parser.add_argument("--mask_threshold", type=float, default=1.0, help="Threshold for mask to be used as 1")
-    parser.add_argument("--heatmap_backprop", action = 'store_true')
+    parser.add_argument("--concept_image", type=str, 
+                        default = '/data7/sooyeon/MyData/perfusion_dataset/td_100/100_td/td_1.jpg')
+    parser.add_argument("--prompt", type=str,
+                        default = 'teddy bear, wearing like a super hero')
+    parser.add_argument("--negtive_prompt", type=str,
+                        default = 'low quality, worst quality, bad anatomy,bad composition, poor, low effort')
+    parser.add_argument("--num_ddim_steps", type=int, default=30)
     args = parser.parse_args()
-
     args = train_util.read_config_from_file(args, parser)
     main(args)
