5d04960aacd8134dd9265808b8f14e37886b2112
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-10-30 17:13:53 +0900

    update

--- image_inverting.sh
+++ image_inverting.sh
@@ -3,8 +3,8 @@ python image_inverting.py --device cuda:5  --process_title parksooyeon \
                           --concept_image_folder /data7/sooyeon/MyData/perfusion_dataset/cat \
                           --prompt 'cat wearing like a wizard' \
                           --negative_prompt 'low quality, worst quality, bad anatomy,bad composition, poor, low effort' \
-                          --output_dir ./result/inference_result/perfusion_experiment/cat/random_init_guidance_8_ddim_50_cross_instead_only_values_without_background_img --seed 42 --guidance_scale 8 \
-                          --num_ddim_steps 50 --min_value 12 --max_self_input_time 10
+                          --output_dir ./result/inference_result/perfusion_experiment/cat/random_init_guidance_8_ddim_50_sef_mean --seed 42 --guidance_scale 8 \
+                          --num_ddim_steps 50 --min_value 12 --max_self_input_time 10 --self_key_control
 
 python image_inverting.py --device cuda:4  --process_title parksooyeon \
                           --pretrained_model_name_or_path /data7/sooyeon/pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned-emaonly.ckpt \
