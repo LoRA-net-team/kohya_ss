dc6453ea7804827504351dc1bf40e3ecd8bfdcb6
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-11-04 20:08:51 +0900

    Update train_network_pretraining.py

--- train_network_pretraining.py
+++ train_network_pretraining.py
@@ -978,7 +978,6 @@ class NetworkTrainer:
                     task_loss = loss
                     attention_losses = {}
                     attention_losses["loss/task_loss"] = loss
-                    print(f'attention_losses elem, loss : {loss}')
 
                     # -------------------------------------------------------------------------------------------------------------------------------------------------
                     # 2) heatmap loss
@@ -1059,7 +1058,6 @@ class NetworkTrainer:
                     p_loss = torch.nn.functional.mse_loss(lora_cond.float(),org_cond.float(),reduction="none")
                     preservating_loss += p_loss.mean()
                 attention_losses["loss/text_preservating_loss"] = preservating_loss.mean()
-                print(f'attention_losses elem, preservating_loss.mean() : {preservating_loss.mean()}')
                 if is_main_process:
                     wandb.log(attention_losses)
                 optimizer.step()
@@ -1104,6 +1102,7 @@ class NetworkTrainer:
                         attention_losses[k] = v.detach().item()
                     else :
                         attention_losses[k] = v
+
                 progress_bar.set_postfix(**logs)
                 if args.scale_weight_norms:
                     progress_bar.set_postfix(**{**max_mean_logs, **logs})
