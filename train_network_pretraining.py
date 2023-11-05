d2b6629c98329f7afd9f6b60126ec484d056a1df
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-11-05 11:42:37 +0900

    Update train_network_pretraining.py

--- train_network_pretraining.py
+++ train_network_pretraining.py
@@ -520,7 +520,7 @@ class NetworkTrainer:
             unwrapped_nw.save_weights(ckpt_file, save_dtype,metadata=None)
 
         print("\n step 13. image inference before training anything")
-        self.sample_images(accelerator, args, -1, 0, accelerator.device, vae, tokenizer, text_encoder, unet,attention_storer=attention_storer,)
+        self.sample_images(accelerator, args, -1, 0, accelerator.device, vae, tokenizer, text_encoder, unet)
 
         print("\n step 14. text encoder lora pretraining")
         pretraining_epochs = args.pretraining_epochs
