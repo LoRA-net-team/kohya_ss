50e2c88417b5bdd731d715c44bd79c018d9073b4
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-11-06 10:50:11 +0900

    cls to 1.5

--- sy.py
+++ sy.py
@@ -64,7 +64,7 @@ def register_attention_control(unet : nn.Module, controller:AttentionStore, mask
                 is_cross_attention = True
                 uncon, con = context.chunk(2)
                 trg_size = torch.ones(con.shape)
-                trg_size[:, 0, :] = 0.6
+                trg_size[:, 0, :] = 1.5
                 con = con * trg_size.to(con.device)  # class_text_embeddings
                 context = torch.cat([uncon, con])
             query = self.to_q(hidden_states)
