8919f7fb20bdd4c5553c38e1f98e6c5e5e2801e8
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-11-06 11:40:42 +0900

    Update lpw_stable_diffusion.py

--- library/lpw_stable_diffusion.py
+++ library/lpw_stable_diffusion.py
@@ -550,7 +550,6 @@ def get_weighted_text_embeddings_reg(
 
         prompt_weights = prompt_weights.squeeze(-1)
         prompt_weights[:, prompt_net_len + 1:] = 0.0
-        prompt_weights = prompt_weights.unsqueeze(-1)
         text_embeddings *= prompt_weights.unsqueeze(-1)
 
 
