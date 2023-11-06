1cf91dc99d6e50cf5ca8192b50430d7ac38c377a
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-11-06 14:04:18 +0900

    inference code change

--- park.py
+++ park.py
@@ -1,18 +1,12 @@
 import os, torch
 from torch.nn.functional import normalize
-org_text_input = torch.tensor([[[1.0,2.0,3.0],
-                                [3.0,4.0,5.0]]])
-norm_text_input = normalize(org_text_input, p=2, dim=2)
-#trg_size = torch.norm(org_text_input, dim=2).unsqueeze(-1)
 
-trg_size = torch.ones(org_text_input.shape)
-trg_size[:,1,:] = 0.8
-print(trg_size)
-#trg_size = trg_size.expand(org_text_input.shape)
-print(trg_size)
-print(org_text_input)
-print(norm_text_input)
-print(trg_size)
-print()
-out = norm_text_input*trg_size
-print(out)
+text_embeddings = torch.randn((1,77,768))
+previous_mean = text_embeddings.float().mean(axis=[-2, -1])
+#print(previous_mean)
+previous_mean = text_embeddings.float().mean()
+#print(previous_mean)
+
+prompt_weights = torch.ones((1,77,1))
+prompt_weights = prompt_weights.squeeze(-1)
+print(prompt_weights.shape)
