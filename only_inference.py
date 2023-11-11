77f7bac777f2424e5a31452fb6ae6afeacb27616
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-11-11 13:10:21 +0900

    only image lora upgrading

--- only_inference.py
+++ only_inference.py
@@ -334,10 +334,13 @@ class NetworkTrainer:
                                 score += 1
                     if score == 0 and 'alpha' not in layer_name:
                         weights_sd[layer_name] = weights_sd[layer_name] * 0
+
                     if 'alpha' in layer_name:
-                        weights_sd[layer_name] = weights_sd[layer_name] * 1.5
-                        alpha_value = weights_sd[layer_name]
-                        print(f'layer_name : {layer_name} , alpha_value : {alpha_value}')
+                        if 'text' in layer_name or 'attn2_to_k' in layer_name or 'attn2_to_v' not in layer_name:
+                            weights_sd[layer_name] = weights_sd[layer_name] * 1
+                        else :
+                            weights_sd[layer_name] = weights_sd[layer_name] * 1.5
+                        #print(f'layer_name : {layer_name} , alpha_value : {alpha_value}')
                     # because alpha is np, should be on cpu
                     #else :
                     #    print(f'layer to use : {layer_name}')
