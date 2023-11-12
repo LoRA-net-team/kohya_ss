5c25ba069e5bc00f2f0921e134d7c6c68dc4658d
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-11-12 12:31:52 +0900

    Update check_lora_size.py

--- check_lora_size.py
+++ check_lora_size.py
@@ -365,7 +365,7 @@ class NetworkTrainer:
 
                 for i, lora_module in enumerate(lora_modules) :
                     lora_name = lora_module.lora_name
-                    if 'attn1_to_k' in lora_name or 'attn1_to_v' in lora_name  :
+                    if 'attn1_to_k' not in lora_name and 'attn1_to_v' not in lora_name and 'text' not in lora_name :
                         org_sd = lora_module.org_module.state_dict()
                         org_weight = org_sd["weight"]#.to(torch.float)
 
